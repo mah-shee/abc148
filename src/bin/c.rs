b18d13acfe51e82669a63fd63be9aac1b14e81fc
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-15 16:48:31 +0900

    Initial commit

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,12 @@
+use num_integer::lcm;
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        a: usize,
+        b: usize,
     }
+    println!("{}", lcm(a, b));
 }
